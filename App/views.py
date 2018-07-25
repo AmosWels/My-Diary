d012908e41b0b53c0a9fee426e55215605d9488f
Welike Amos <amoswelike@gmail.com>
2018-07-25 13:21:06 +0300

    Modify view

--- App/views.py
+++ App/views.py
@@ -1,4 +1,4 @@
-from flask import  request, jsonify, make_response
+from flask import Flask, request, jsonify, make_response
 '''Initialising a flask application'''
 app = Flask(__name__)
 '''Initialising an empty dictionary'''
