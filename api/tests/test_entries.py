2da45e0a9ad75e61dbcd88d7b17992ea2ced5690
Welike Amos <amoswelike@gmail.com>
2018-08-01 19:44:41 +0300

    Modify Tests

--- api/tests/test_entries.py
+++ api/tests/test_entries.py
@@ -34,8 +34,13 @@ entry2= {
 }
 
 entry3= {
-    "due_date": "02-sep-2001",
-    "name": "Meet Peter",
-    "purpose": "Lorem Ipsum ",
-    "type": "sports"
+    "name": "cancel all flight plans",
+    "due_date": "02-sep-2018",
+    "type": "office",
+    "purpose": "Lorem Ipsum",
+    "user_id": 3
+    # "due_date": "02-sep-2001",
+    # "name": "Meet Peter",
+    # "purpose": "Lorem Ipsum ",
+    # "type": "sports"
 }
