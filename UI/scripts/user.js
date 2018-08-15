1ad68c194169a5a05ebb1115bae0eb06f09b0f75
Welike Amos <amoswelike@gmail.com>
2018-08-15 14:10:04 +0300

    Finish login  UI validation

--- UI/scripts/user.js
+++ UI/scripts/user.js
@@ -4,29 +4,33 @@ function signup(e) {
     var url = 'http://127.0.0.1:5000/api/v1/auth/signup';
     var Pusername = document.getElementById("username1").value;
     var Ppassword = document.getElementById("password1").value;
-    // var password2 = document.getElementById('password2').value;
-    fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-type': 'application/json'
-        },
-        body: JSON.stringify({
-            username: Pusername, password: Ppassword
+    var Ppassword2 = document.getElementById("password2").value;
+    if (Ppassword == Ppassword2){
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: Pusername, password: Ppassword
+            })
         })
-    })
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function(data){
-        if (data.Message === "Created Succesfully"){
-            document.getElementById("call").innerHTML = "Success :" + data.Message;
-            window.location.href = "./index.html"
-        } else {
-            document.getElementById("call").innerHTML = "Fail :" + data.Message;
-            }
-    })
-    .catch(function (error) {  
-        console.log('Request failure: ', error);  
-    });
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function(data){
+            if (data.Message === "Created Succesfully"){
+                document.getElementById("call").innerHTML = "Success :" + data.Message;
+                window.location.href = "./index.html"
+            } else {
+                document.getElementById("call").innerHTML = "Fail :" + data.Message;
+                }
+        })
+        .catch(function (error) {  
+            console.log('Request failure: ', error);  
+        });
+    } else {
+        document.getElementById("call").innerHTML = "Fail : Your passwords Don't match";
+    }
 }
 function signin() {}
