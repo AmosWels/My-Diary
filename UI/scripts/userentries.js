90f1900efc50bc62afbf405bd2306bc543ef6d9f
Welike Amos <amoswelike@gmail.com>
2018-08-16 15:44:44 +0300

    Modify view all entries

--- UI/scripts/userentries.js
+++ UI/scripts/userentries.js
@@ -3,7 +3,7 @@ function getentries(e) {
     e.preventDefault();
     var Token = sessionStorage.getItem('token');
     var url = 'http://127.0.0.1:5000/api/v1/entries';
-    
+
     fetch(url, {
         method: 'GET',
         headers: {
@@ -16,26 +16,37 @@ function getentries(e) {
         })
         .then(function (data) {
             if (data.Message === "You haven't created any entries yet. Please create first.") {
-                document.getElementById('result').innerHTML = "Success :" + data.Message;
-            }
-            if (data.entries != "") {
+                document.getElementById("result").innerHTML = "Message : " + data.Message;
+            } else if (data.entries != "") {
                 var object = data.entries;
+                var table = document.getElementsByTagName("table")[0];
+
                 var objectlength = object.length;
-                var table = document.getElementById("customers");
                 for (var i = 0; i < objectlength; i++) {
-                    var record = document.createElement("tr");
+                    var id = object[i].id;
                     var name = object[i].name;
                     var due_date = object[i].due_date;
                     var type = object[i].type;
                     var purpose = object[i].purpose;
                     var date_created = object[i].date_created;
 
-                    document.getElementById("name").innerHTML = name;
-                    document.getElementById("datecreated").innerHTML = date_created;
-                    document.getElementById("type").innerHTML = type;
-                    document.getElementById("purpose").innerHTML = purpose;
-                    document.getElementById("duedate").innerHTML = due_date;
+                    var newRow = table.insertRow(table.rows.length);
+                    var cel1 = newRow.insertCell(0);
+                    var cel2 = newRow.insertCell(1);
+                    var cel3 = newRow.insertCell(2);
+                    var cel4 = newRow.insertCell(3);
+                    var cel5 = newRow.insertCell(4);
+                    var cel6 = newRow.insertCell(5);
+
+                    cel1.innerHTML = id;
+                    cel2.innerHTML = name;
+                    cel3.innerHTML = due_date;
+                    cel4.innerHTML = type;
+                    cel5.innerHTML = purpose;
+                    cel6.innerHTML = date_created;
                 }
+            } else {
+                document.getElementById("result").innerHTML = "Message : " + data.Msg;
             };
         });
 }
