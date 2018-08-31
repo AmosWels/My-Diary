1e4932f7a18f25802d7dc069a29b31fabb630987
Welike Amos <amoswelike@gmail.com>
2018-08-31 19:18:42 +0300

    add logout

--- UI/scripts/main.js
+++ UI/scripts/main.js
@@ -1,2 +1,7 @@
  const baseurl = 'https://mydiario3.herokuapp.com';
 // const baseurl = 'http://127.0.0.1:5000';
+
+function logout() {
+	localStorage.removeItem('token');
+	window.location.href = 'index.html';
+}
